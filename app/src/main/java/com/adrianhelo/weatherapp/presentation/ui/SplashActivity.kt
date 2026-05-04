81bfac307d5e0ae2ce300c07188bd55adec08036
AdrianHeLo <adriankenobi21@gmail.com>
2026-05-04 13:37:43 -0700

    feat(SplashActivity): Add SplashActivity.

---
+++ app/src/main/java/com/adrianhelo/weatherapp/presentation/ui/SplashActivity.kt
@@ -0,0 +1,95 @@
+package com.adrianhelo.weatherapp.presentation.ui
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.adrianhelo.weather.R
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.delay
+
+@AndroidEntryPoint
+class SplashActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            SplashScreen(
+                onFinish = {
+                    startActivity(Intent(this, MainActivity::class.java))
+                    finish()
+                }
+            )
+        }
+    }
+}
+
+@Composable
+fun SplashScreen(onFinish: () -> Unit){
+
+    LaunchedEffect(Unit) {
+        delay(2000)
+        onFinish()
+    }
+
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(
+                brush = Brush.horizontalGradient(
+                    colors = listOf(Color(0xFF59469D), Color(0xFF643D67))
+                )
+            ),
+        contentAlignment = Alignment.Center
+    ) {
+
+        Column (horizontalAlignment = Alignment.CenterHorizontally){
+            Image(
+                painter = painterResource(id = R.drawable.weather_logo),
+                contentDescription = "Logo",
+                modifier = Modifier.size(150.dp)
+            )
+
+            Text(
+                text = "Fetching the currently weather...",
+                fontSize = 20.sp,
+                color = Color.White
+            )
+
+            Spacer(modifier = Modifier.height(150.dp))
+
+            Image(
+                painter = painterResource(id = R.drawable.openweather),
+                contentDescription = "Open Weather",
+                modifier = Modifier.size(100.dp)
+            )
+
+            Text(
+                text = "Powered by Open Weather API.",
+                fontSize = 15.sp,
+                color = Color.White
+            )
+
+        }
+    }
+}
